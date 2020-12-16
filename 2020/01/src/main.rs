8984705c35ee5e7c21b1cc6d3dfc491332f789a4
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2020-12-16 18:05:26 -0500

    update day 1

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -1,8 +1,20 @@
-use std::fs::File;
 use std::error::Error;
+use std::fs::File;
 use std::io::Read;
+use itertools::Itertools;
+
+fn find_combo_mul(nums: &Vec<u32>, n: usize, target: u32) -> u32 {
+  nums.iter()
+      .combinations(n)
+      .find(|group| group.iter().map(|n| **n).sum::<u32>() == target)
+      .unwrap()
+      .iter().map(|n| **n)
+      .product::<u32>()
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
+  let target = 2020;
+
   let mut input = String::new();
   File::open("./input/problem")?.read_to_string(&mut input)?;
 
@@ -10,40 +22,8 @@ fn main() -> Result<(), Box<dyn Error>> {
       .map(|num| num.parse::<u32>())
       .collect::<Result<Vec<_>, _>>()?;
 
-  let part_one = nums.iter().enumerate()
-      .flat_map(|(index, val)| {
-        nums[index + 1..].iter().map(move |n| (val, n))
-      })
-      .find_map(|(a, b)| {
-        if a + b == 2020 {
-          Some(a * b)
-        } else {
-          None
-        }
-      }).unwrap_or(0);
-
-  let part_two = nums.iter().enumerate()
-      .flat_map(|(a_index, a)| {
-        nums[a_index + 1..].iter()
-            .enumerate()
-            .map(move |(b_index, b)| ((a_index, a), (b_index + 1, b)))
-      })
-      .flat_map(|((a_index, a), (b_index, b))| {
-        nums.iter()
-            .enumerate()
-            .filter(move |(c_index, c)| c_index != &a_index && c_index != &b_index)
-            .map(move |(c_index, c)| (a, b, c))
-      })
-      .find_map(|(a, b, c)| {
-        if a + b + c == 2020 {
-          Some(a * b * c)
-        } else {
-          None
-        }
-      }).unwrap_or(0);
-
-  println!("{:?}", part_one);
-  println!("{:?}", part_two);
+  println!("{}", find_combo_mul(&nums, 2, target));
+  println!("{}", find_combo_mul(&nums, 3, target));
 
   Ok(())
 }
