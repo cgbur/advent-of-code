ae76e23b9807900576f00d4100ce8d66c8e1614c
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2021-12-06 00:36:54 -0500

    06

---
+++ 2021/06/src/main.rs
@@ -0,0 +1,47 @@
+use itertools::Itertools;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+
+const FISHY_REPRODUCTION_MAX: usize = 9;
+const FISHY_SIZE: usize = FISHY_REPRODUCTION_MAX + 1;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let inputs = parse_input()?;
+
+    println!("{}", sim(&mut inputs.clone(), 80));
+    println!("{}", sim(&mut inputs.clone(), 256));
+    Ok(())
+}
+
+fn sim(fish_counts: &mut [u64], days: usize) -> u64 {
+    for _ in 0..days {
+        fish_counts[9] += fish_counts[0];
+        fish_counts[7] += fish_counts[0];
+        for i in 0..fish_counts.len() {
+            fish_counts[i] = *fish_counts.get(i + 1).unwrap_or(&0);
+        }
+    }
+
+    fish_counts.iter().sum()
+}
+
+fn parse_input() -> Result<[u64; FISHY_SIZE], std::io::Error> {
+    let mut input = String::new();
+    File::open("./input")?.read_to_string(&mut input)?;
+
+    let mut fish_counts = [0; FISHY_SIZE];
+    let fishies = input
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect_vec();
+
+    for f in fishies {
+        fish_counts[f as usize] += 1;
+    }
+
+    Ok(fish_counts)
+}
