9a0e10069915f6fc3b5b7f3803fa33fb1a9b515f
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2021-12-06 02:32:53 -0500

    update

--- 2021/06/src/main.rs
+++ 2021/06/src/main.rs
@@ -2,24 +2,22 @@ use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 
-const FISHY_REPRODUCTION_MAX: usize = 9;
+const FISHY_REPRODUCTION_MAX: usize = 8;
+const FISHY_REPRODUCTION: usize = 6;
 const FISHY_SIZE: usize = FISHY_REPRODUCTION_MAX + 1;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let inputs = parse_input()?;
+    let mut inputs = parse_input()?;
 
     println!("{}", sim(&mut inputs.clone(), 80));
-    println!("{}", sim(&mut inputs.clone(), 256));
+    println!("{}", sim(&mut inputs, 256));
     Ok(())
 }
 
 fn sim(fish_counts: &mut [u64], days: usize) -> u64 {
     for _ in 0..days {
-        fish_counts[9] += fish_counts[0];
-        fish_counts[7] += fish_counts[0];
-        for i in 0..fish_counts.len() {
-            fish_counts[i] = *fish_counts.get(i + 1).unwrap_or(&0);
-        }
+        fish_counts.rotate_left(1);
+        fish_counts[FISHY_REPRODUCTION] += fish_counts[FISHY_REPRODUCTION_MAX];
     }
 
     fish_counts.iter().sum()
