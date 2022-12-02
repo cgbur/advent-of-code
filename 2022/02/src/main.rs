562323566e307124fb2c55267f63b78767a4d9d8
Chris Burgess <cgburgess10@gmail.com>
2022-12-02 00:26:24 -0500

    day 2

---
+++ 2022/02/src/main.rs
@@ -0,0 +1,129 @@
+use itertools::Itertools;
+use std::{fs::File, io::Read};
+
+fn main() {
+    let mut file = File::open("input").expect("Failed to open input.txt");
+    let mut buf = String::new();
+    file.read_to_string(&mut buf)
+        .expect("Failed to read input.txt");
+
+    let score_1 = buf
+        .split("\n")
+        .map(|elf| {
+            let (op, me) = elf.split(" ").collect_tuple().unwrap();
+            rock_paper_sciccors(op, me)
+        })
+        .sum::<u32>();
+
+    let score_2 = buf
+        .split("\n")
+        .map(|elf| {
+            let (op, me) = elf.split(" ").collect_tuple().unwrap();
+            rock_paper_sciccors_2(op, me)
+        })
+        .sum::<u32>();
+
+    println!("Part 1: {}", score_1);
+    println!("Part 2: {}", score_2);
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Choice {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Winner {
+    Me,
+    Opponent,
+    Tie,
+}
+
+fn rock_paper_sciccors_2(opponent: &str, me: &str) -> u32 {
+    // A rock B is paper C is sciccors
+    // right is X rock B is paper C is sciccors
+    let op = match opponent {
+        "A" => Choice::Rock,
+        "B" => Choice::Paper,
+        "C" => Choice::Scissors,
+        _ => panic!("Invalid input"),
+    };
+
+    let me = match me {
+        "X" => Winner::Opponent,
+        "Y" => Winner::Tie,
+        "Z" => Winner::Me,
+        _ => panic!("Invalid input"),
+    };
+
+    let to_play_to_fix_outcome = match op {
+        Choice::Rock => match me {
+            Winner::Opponent => Choice::Scissors,
+            Winner::Tie => Choice::Rock,
+            Winner::Me => Choice::Paper,
+        },
+        Choice::Paper => match me {
+            Winner::Opponent => Choice::Rock,
+            Winner::Tie => Choice::Paper,
+            Winner::Me => Choice::Scissors,
+        },
+        Choice::Scissors => match me {
+            Winner::Opponent => Choice::Paper,
+            Winner::Tie => Choice::Scissors,
+            Winner::Me => Choice::Rock,
+        },
+    };
+
+    let score = match me {
+        Winner::Me => 6,
+        Winner::Opponent => 0,
+        Winner::Tie => 3,
+    } + match to_play_to_fix_outcome {
+        Choice::Rock => 1,
+        Choice::Paper => 2,
+        Choice::Scissors => 3,
+    };
+
+    score
+}
+
+fn rock_paper_sciccors(opponent: &str, me: &str) -> u32 {
+    // A rock B is paper C is sciccors
+    // right is X rock B is paper C is sciccors
+    let op = match opponent {
+        "A" => Choice::Rock,
+        "B" => Choice::Paper,
+        "C" => Choice::Scissors,
+        _ => panic!("Invalid input"),
+    };
+    let me = match me {
+        "X" => Choice::Rock,
+        "Y" => Choice::Paper,
+        "Z" => Choice::Scissors,
+        _ => panic!("Invalid input"),
+    };
+
+    let winner = match (me, op) {
+        (Choice::Rock, Choice::Paper) => Winner::Opponent,
+        (Choice::Rock, Choice::Scissors) => Winner::Me,
+        (Choice::Paper, Choice::Rock) => Winner::Me,
+        (Choice::Paper, Choice::Scissors) => Winner::Opponent,
+        (Choice::Scissors, Choice::Rock) => Winner::Opponent,
+        (Choice::Scissors, Choice::Paper) => Winner::Me,
+        _ => Winner::Tie,
+    };
+
+    let score = match winner {
+        Winner::Me => 6,
+        Winner::Opponent => 0,
+        Winner::Tie => 3,
+    } + match me {
+        Choice::Rock => 1,
+        Choice::Paper => 2,
+        Choice::Scissors => 3,
+    };
+
+    score
+}
