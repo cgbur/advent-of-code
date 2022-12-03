304bc33c895b2fbcea1fb993dbddd2c10b212786
Chris Burgess <cgburgess10@gmail.com>
2022-12-03 15:10:51 -0500

    min

--- 2022/03/src/main.rs
+++ 2022/03/src/main.rs
@@ -1,40 +1,31 @@
 use aoc::input;
 use itertools::Itertools;
-use std::collections::HashSet;
 
 fn main() {
     let part_one = input()
         .lines()
         .map(|line| {
-            let middle = line.len() / 2;
-            let (a, b) = line.split_at(middle);
-            let a = a.chars().collect::<HashSet<_>>();
-            let b = b.chars().collect::<HashSet<_>>();
-            let common = *a.intersection(&b).next().unwrap();
-            score(common)
+            let (a, b) = line.split_at(line.len() / 2);
+            score(&intersect(a, b))
         })
         .sum::<u32>();
 
     let part_two = input()
         .lines()
-        .chunks(3)
-        .into_iter()
-        .map(|a| {
-            let (a, b, c) = a.collect_tuple().unwrap();
-            let a = a.chars().collect::<HashSet<_>>();
-            let b = b.chars().collect::<HashSet<_>>();
-            let c = c.chars().collect::<HashSet<_>>();
-            let common = a.intersection(&b).cloned().collect::<HashSet<_>>();
-            let common = *common.intersection(&c).next().unwrap();
-            score(common)
-        })
+        .tuples()
+        .map(|(a, b, c)| score(&intersect(&intersect(a, b), c)))
         .sum::<u32>();
 
     println!("{:?}", part_one);
     println!("{:?}", part_two);
 }
 
-fn score(c: char) -> u32 {
+fn intersect(a: &str, b: &str) -> String {
+    a.chars().filter(|&c| b.contains(c)).collect()
+}
+
+fn score(c: &str) -> u32 {
+    let c = c.chars().next().unwrap();
     match c {
         'a'..='z' => c as u32 - 'a' as u32 + 1,
         'A'..='Z' => c as u32 - 'A' as u32 + 27,
