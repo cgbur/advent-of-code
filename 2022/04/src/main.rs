01314ad398f2337b6dbc6d58ff41156fcc9e4a00
Chris Burgess <cgburgess10@gmail.com>
2022-12-04 00:27:08 -0500

    day 4 ez

--- 2022/04/src/main.rs
+++ 2022/04/src/main.rs
@@ -1,12 +1,64 @@
+use std::str::FromStr;
+
 use aoc::input;
 use itertools::Itertools;
 
+#[derive(Debug, Clone, Copy)]
+struct Range {
+    start: u32,
+    stop: u32,
+}
+
+impl Range {
+    fn overlaps(&self, other: Range) -> bool {
+        self.start <= other.stop && self.stop >= other.start
+    }
+
+    fn contains(&self, other: Range) -> bool {
+        self.start <= other.start && self.stop >= other.stop
+    }
+}
+
+impl FromStr for Range {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (start, stop) = s.split_once('-').unwrap();
+        Ok(Range {
+            start: start.parse().unwrap(),
+            stop: stop.parse().unwrap(),
+        })
+    }
+}
+
+struct WorkOrder(Range, Range);
+
+impl FromStr for WorkOrder {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (left, right) = s.split_once(',').unwrap();
+        Ok(WorkOrder(left.parse().unwrap(), right.parse().unwrap()))
+    }
+}
+
 fn main() {
-    let part_one = input();
+    let part_one = input()
+        .lines()
+        .filter(|line| {
+            let WorkOrder(left, right) = line.parse().unwrap();
+            left.contains(right) || right.contains(left)
+        })
+        .count();
+
+    let part_two = input()
+        .lines()
+        .filter(|line| {
+            let WorkOrder(left, right) = line.parse().unwrap();
+            left.overlaps(right)
+        })
+        .count();
 
     println!("{:?}", part_one);
-
-    // let part_two = input();
-
-    // println!("{:?}", part_two);
+    println!("{:?}", part_two);
 }
