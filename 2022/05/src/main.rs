2981e3f3d0b613f0f1572340d15c05bbcc471353
Chris Burgess <cgburgess10@gmail.com>
2022-12-05 22:10:51 -0500

    day5

---
+++ 2022/05/src/main.rs
@@ -0,0 +1,59 @@
+use aoc::{input, Aoc};
+use itertools::Itertools;
+
+fn main() {
+    let mut inputs = input().groups();
+    let mut stacks = vec![vec![]; 9];
+    let state = inputs.next().unwrap();
+    let commands = inputs.next().unwrap();
+
+    state.lines().take(8).for_each(|line| {
+        (0..9)
+            .map(|pos| (pos, 4 * pos + 1))
+            .for_each(|(stack, pos)| {
+                let c = line.chars().nth(pos).unwrap();
+                if c != ' ' {
+                    stacks[stack].push(c);
+                }
+            });
+    });
+
+    for stack in &mut stacks {
+        stack.reverse();
+    }
+
+    let stacks_clone = stacks.clone();
+    for command in commands.lines() {
+        // extract 3 numbers from th eline
+        let (count, from, to) = command
+            .split_whitespace()
+            .filter_map(|s| s.parse::<usize>().ok())
+            .collect_tuple()
+            .unwrap();
+        for _ in 0..count {
+            let block = stacks[from - 1].pop().unwrap();
+            stacks[to - 1].push(block);
+        }
+    }
+
+    let part_one = stacks.iter().map(|s| s.last().unwrap()).join("");
+    println!("Part one: {}", part_one);
+
+    let mut stacks = stacks_clone;
+    for command in commands.lines() {
+        // extract 3 numbers from th eline
+        let (count, from, to) = command
+            .split_whitespace()
+            .filter_map(|s| s.parse::<usize>().ok())
+            .collect_tuple()
+            .unwrap();
+        for _ in 0..count {
+            let block = stacks[from - 1].pop().unwrap();
+            stacks[to - 1].push(block);
+        }
+        let stack_len = stacks[to - 1].len();
+        stacks[to - 1][stack_len - count..].reverse();
+    }
+    let part_two = stacks.iter().map(|s| s.last().unwrap()).join("");
+    println!("Part two: {}", part_two);
+}
